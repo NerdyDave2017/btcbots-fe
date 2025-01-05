d3976e09da2d6a99af0553e79eea1abd1de43932
NerdyDave2017 <nerdydave2017@gmail.com>
2025-01-05 19:37:50 +0100

    feat: itegrate forgot password API

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,8 +1,40 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Input from "../../components/input";
 import Button from "../../components/button";
+import { useForgotPassword } from "@/src/hooks/postRequests";
+import { isAxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const page = () => {
+  const [email, setEmail] = useState("");
+  const router = useRouter();
+  const { mutate: ForgotPassword, isPending } = useForgotPassword();
+
+  const handleForgotPassword = async () => {
+    ForgotPassword(email, {
+      onSuccess: () => {
+        toast.success("Password reset link sent to your email", {
+          position: "top-center",
+        });
+        router.push("/login");
+      },
+      onError: (error: Error) => {
+        if (isAxiosError(error)) {
+          toast.error(error.response?.data.message, {
+            position: "top-center",
+          });
+        } else {
+          console.error(error);
+          toast.error("An unexpected error occurred", {
+            position: "top-center",
+          });
+        }
+      },
+    });
+  };
+
   return (
     <div className="w-full flex flex-col items-center justify-center py-[70px] px-[160px] gap-[48px] text-text-light">
       <div className="w-[475px]">
@@ -13,12 +45,14 @@ const page = () => {
       </div>
 
       <div className="w-[475px] flex flex-col gap-[32px]">
-        <Input label="Email" icon="email" value="" setValue={() => {}} />
+        <Input label="Email" icon="email" value={email} setValue={setEmail} />
 
         <Button
+          onClick={handleForgotPassword}
           text="Submit"
           className="w-full flex text-text-dark"
           size="lg"
+          loading={isPending}
         />
       </div>
     </div>
