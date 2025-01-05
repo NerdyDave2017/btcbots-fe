39b9d5ce86c0b4b2c13773ca8d348619fa3f45c2
NerdyDave2017 <nerdydave2017@gmail.com>
2025-01-05 19:45:18 +0100

    feat: integrate reset password API

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,8 +1,58 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Input from "../../components/input";
 import Button from "../../components/button";
+import { useResetPassword } from "@/src/hooks/postRequests";
+import { isAxiosError } from "axios";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const page = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const { mutate: ResetPassword, isPending } = useResetPassword();
+
+  // Get individual query parameters by key
+  const otp = searchParams.get("otp") || "";
+
+  const handleChangePassword = async () => {
+    if (password !== confirmPassword) {
+      toast.error("New password must match Confirm password", {
+        position: "top-center",
+      });
+      return;
+    }
+    ResetPassword(
+      {
+        password,
+        otp,
+      },
+      {
+        onSuccess: () => {
+          toast.success("Password Reset", { position: "top-center" });
+          router.push("/dashboard/home");
+        },
+        onError: (error: Error) => {
+          if (isAxiosError(error)) {
+            toast.error(error.response?.data.message, {
+              position: "top-center",
+            });
+          } else {
+            console.error(error);
+            toast.error("An unexpected error occurred", {
+              position: "top-center",
+            });
+          }
+        },
+      }
+    );
+  };
+
   return (
     <div className="w-full flex flex-col items-center justify-center py-[70px] px-[160px] gap-[48px] text-text-light">
       <div className="w-[475px]">
@@ -16,20 +66,22 @@ const page = () => {
         <Input
           label="New Password"
           icon="password"
-          value=""
-          setValue={() => {}}
+          value={password}
+          setValue={setPassword}
         />
         <Input
           label="Confirm New Password"
           icon="password"
-          value=""
-          setValue={() => {}}
+          value={confirmPassword}
+          setValue={setConfirmPassword}
         />
 
         <Button
+          onClick={handleChangePassword}
           text="Continue"
           className="w-full flex text-text-dark"
           size="lg"
+          loading={isPending}
         />
       </div>
     </div>
