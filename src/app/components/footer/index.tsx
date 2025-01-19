2bf4e1bd9d57353d12729a207a7ac2148800d9ac
NerdyDave2017 <nerdydave2017@gmail.com>
2025-01-19 06:25:41 +0100

    feat: make footer component responsive

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -7,69 +7,57 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
-  const socials = [
-    {
-      name: "Facebook",
-      icon: Facebook,
-      link: "https://www.facebook.com/",
-    },
-    {
-      name: "Instagram",
-      icon: Instagram,
-      link: "https://www.instagram.com/",
-    },
-    {
-      name: "TikTok",
-      icon: TikTok,
-      link: "https://www.tiktok.com/",
-    },
-    {
-      name: "YouTube",
-      icon: YouTube,
-      link: "https://www.youtube.com/",
-    },
-    {
-      name: "X",
-      icon: X,
-      link: "https://www.X.com/",
-    },
-  ];
+  // const socials = [
+  //   {
+  //     name: "Facebook",
+  //     icon: Facebook,
+  //     link: "https://www.facebook.com/",
+  //   },
+  //   {
+  //     name: "Instagram",
+  //     icon: Instagram,
+  //     link: "https://www.instagram.com/",
+  //   },
+  //   {
+  //     name: "TikTok",
+  //     icon: TikTok,
+  //     link: "https://www.tiktok.com/",
+  //   },
+  //   {
+  //     name: "YouTube",
+  //     icon: YouTube,
+  //     link: "https://www.youtube.com/",
+  //   },
+  //   {
+  //     name: "X",
+  //     icon: X,
+  //     link: "https://www.X.com/",
+  //   },
+  // ];
 
   return (
     <div className="py-[70px] bg-dark-background text-text-dark">
       <Container>
-        <div className="w-full flex items-start justify-between">
-          <div className="">
+        <div className="w-full flex flex-col md:flex-row items-start justify-between">
+          <div className="mb-12">
             <Image className="mt-[5px] w-[130px]" src={MainLogo} alt="Logo" />
           </div>
           <div className="">
-            <nav className="h-[148px] flex justify-center items-start gap-12 text-[#ebeef1] text-lg font-normal">
-              <Link href="/bots">Bots</Link>
-              <Link href="/store">Store</Link>
-              <Link href="/blog">Blog</Link>
-              <Link href="/features">Features</Link>
+            <nav className="flex flex-col md:flex-row justify-center items-start gap-12 text-[#ebeef1] text-lg font-normal">
+              <Link href="/">Home</Link>
+              <Link href="/">About Us</Link>
+              <Link href="/">Contact Us</Link>
               <div className="flex flex-col gap-6">
-                <div className="text-[#ebeef1] text-lg font-normal">
-                  Company
-                </div>
-                <div className="flex flex-col gap-4">
-                  <Link href="/contact">Contact Us</Link>
-                  <Link href="/about">About Us</Link>
-                </div>
-              </div>
-              <div className="flex flex-col gap-6">
-                <div className="text-[#ebeef1] text-lg font-normal">
-                  Learn more
-                </div>
+                <div className="text-main text-lg font-normal">Learn more</div>
                 <div className="flex flex-col gap-4">
                   <Link href="https://usdbots.com">USDbots.com</Link>
                   <Link href="/knowledge-base">Knowledge Base</Link>
-                  <Link href="/performance">Performance</Link>
+                  {/* <Link href="/performance">Performance</Link> */}
                 </div>
               </div>
             </nav>
           </div>
-          <div className="">
+          {/* <div className="">
             <p className="text-lg mb-[16px]">Connect with us</p>
             <div className="flex">
               {socials.map((social, index) => (
@@ -84,7 +72,7 @@ const Footer = () => {
                 </Link>
               ))}
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="text-center text-[#ebeef1] text-base font-light mt-[60px]">
           <p className="">© 2024 Cryptobots.io LLC Miami Beach, FL 33141</p>{" "}
