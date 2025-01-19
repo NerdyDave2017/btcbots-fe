0c3b04900c8832fce0f1d9c7fdfe07f366d9dbba
NerdyDave2017 <nerdydave2017@gmail.com>
2025-01-19 01:37:27 +0100

    feat: make how component responsive

--- src/app/landing/how/index.tsx
+++ src/app/landing/how/index.tsx
@@ -7,15 +7,15 @@ const How = () => {
     <div className="text-text-light py-[70px]">
       <Container>
         <div className="w-[880px] mx-auto flex flex-col items-center justify-center gap-[50px]">
-          <p className="text-main text-[40px]">How it works?</p>
-          <div className="w-full flex items-center justify-between">
+          <p className="text-main text-[28px] xl:text-[40px]">How it works?</p>
+          <div className="w-full flex flex-col md:flex-row  items:start md:items-center justify-between gap-6">
             <div className="">
-              <p className="text-2xl">
+              <p className="text-lg md:text-xl xl:text-2xl">
                 Completely <br />
                 <span className="text-main">FREE</span>
               </p>
             </div>
-            <div className="w-[540px] text-2xl">
+            <div className="max-w-[540px] text-lg md:text-xl xl:text-2xl">
               <p className=" mb-[24px]">
                 We want to prove to you, our system really works! Thats why
                 connecting and using cryptobots.io is simple and completely
@@ -24,7 +24,7 @@ const How = () => {
               <HowBar />
             </div>
           </div>
-          <div className="w-full h-[512px] rounded-lg bg-[#D9D9D9]"></div>
+          <div className="w-full h-[220px] md:h-[400px]  xl:h-[512px] rounded-lg bg-[#D9D9D9]"></div>
         </div>
       </Container>
     </div>
